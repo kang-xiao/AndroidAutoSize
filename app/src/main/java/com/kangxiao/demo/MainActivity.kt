67ec8af707bd07c5a4c5445926d3c4c2cbb387d6
kang-xiao <diao_kx@163.com>
2022-12-27 14:58:08 +0800

    fix:demo取消androidx

--- app/src/main/java/com/kangxiao/demo/MainActivity.kt
+++ app/src/main/java/com/kangxiao/demo/MainActivity.kt
@@ -2,9 +2,9 @@ package com.kangxiao.demo
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.kangxiao.autosize.AutoSizeConfig
 
 class MainActivity : AppCompatActivity() {
